b3d9ff972c9af1f5d01c2ac4c7e97dd32e620b74
Vijesh Ghandare <ghandarevb14.comp@coep.ac.in>
2024-08-26 00:02:48 +0530

    updated setup.py with license

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='password_generator',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     description='A simple password generator package',
     author='Vijesh Ghandare',
     author_email='mail.vijeshg@gmail.com',
     url='https://github.com/VIJESHG/password_generator',
     install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
 )
